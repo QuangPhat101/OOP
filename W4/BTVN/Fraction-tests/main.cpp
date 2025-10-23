5420127a9747e84293af5ae54a91c046e8410541
QuangPhat101 <nguyenqphat123@gmail.com>
2025-10-23 09:24:27 +0700

    update-tests

--- W4/BTVN/Fraction-tests/main.cpp
+++ W4/BTVN/Fraction-tests/main.cpp
@@ -6,6 +6,86 @@
 #include "../Fraction/usecase/CalcSumFractionsUseCase.h"
 
 
+TEST(FractionMinimalist, ReducePositive)
+{
+    Fraction f(2, 4);
+    bus::getMinimalistFractions(f);
+    EXPECT_EQ(f.getNumerator(), 1);
+    EXPECT_EQ(f.getDenominator(), 2);
+}
+
+TEST(FractionMinimalist, NegativeDenominator)
+{
+    Fraction f(1, -2);
+    bus::getMinimalistFractions(f);
+    EXPECT_EQ(f.getNumerator(), -1);
+    EXPECT_EQ(f.getDenominator(), 2);
+}
+
+TEST(FractionMinimalist, ZeroNumerator)
+{
+    Fraction f(0, 5);
+    bus::getMinimalistFractions(f);
+    EXPECT_EQ(f.getNumerator(), 0);
+    EXPECT_EQ(f.getDenominator(), 1);
+}
+
+TEST(FractionMinimalist, AlreadyReduced)
+{
+    Fraction f(3, 7);
+    bus::getMinimalistFractions(f);
+    EXPECT_EQ(f.getNumerator(), 3);
+    EXPECT_EQ(f.getDenominator(), 7);
+}
+
+TEST(FractionMinimalist, BothNegative)
+{
+    Fraction f(-2, -4);
+    bus::getMinimalistFractions(f);
+    EXPECT_EQ(f.getNumerator(), 1);
+    EXPECT_EQ(f.getDenominator(), 2);
+}
+
+TEST(FractionMinimalist, LargeGCD)
+{
+    Fraction f(120, 90);
+    bus::getMinimalistFractions(f);
+    EXPECT_EQ(f.getNumerator(), 4);
+    EXPECT_EQ(f.getDenominator(), 3);
+}
+
+// CalcSumFractions tests
+
+TEST(FractionSum, SimpleAdd)
+{
+    Fraction a(1, 2);
+    Fraction b(1, 3);
+    Fraction r = bus::CalcSumFractions(a, b);
+    bus::getMinimalistFractions(r);
+    EXPECT_EQ(r.getNumerator(), 5);
+    EXPECT_EQ(r.getDenominator(), 6);
+}
+
+TEST(FractionSum, SumCancelToZero)
+{
+    Fraction a(-1, 2);
+    Fraction b(1, 2);
+    Fraction r = bus::CalcSumFractions(a, b);
+    bus::getMinimalistFractions(r);
+    EXPECT_EQ(r.getNumerator(), 0);
+    EXPECT_EQ(r.getDenominator(), 1);
+}
+
+TEST(FractionSum, SumWithReduction)
+{
+    Fraction a(1, 4);
+    Fraction b(1, 4);
+    Fraction r = bus::CalcSumFractions(a, b);
+    bus::getMinimalistFractions(r);
+    EXPECT_EQ(r.getNumerator(), 1);
+    EXPECT_EQ(r.getDenominator(), 2);
+}
+
 
 int main(int argc, char**argv){
     testing::InitGoogleTest(&argc, argv);
